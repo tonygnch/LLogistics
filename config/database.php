ed6e6d95696faed2027633dc03ebcab480d0e799
Tony <toni.ganchev@me.com>
2019-02-12 01:08:39 +0200

    update db config

--- config/database.php
+++ config/database.php
@@ -42,7 +42,7 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '51.15.136.16'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'llogistics'),
             'username' => env('DB_USERNAME', 'root'),
