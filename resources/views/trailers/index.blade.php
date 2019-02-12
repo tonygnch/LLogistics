97eef9c94bfa696b773b5afba8053a10d77b6a82
Tony <toni.ganchev@me.com>
2019-02-12 02:58:55 +0200

    minor updates

--- resources/views/trailers/index.blade.php
+++ resources/views/trailers/index.blade.php
@@ -4,11 +4,6 @@
         <div class="col-md-12">
             <section class="panel">
                 <header class="panel-heading">
-                    <div class="panel-actions">
-                        <a href="#" class="fa fa-caret-down"></a>
-                        <a href="#" class="fa fa-times"></a>
-                    </div>
-
                     <h2 class="panel-title">Actions</h2>
                 </header>
                 <div class="panel-body">
--- resources/views/trucks/modify.blade.php
+++ resources/views/trucks/modify.blade.php
@@ -5,11 +5,6 @@
             <form class="form-horizontal" action="{{ route('modifyTruck', $data->id) }}" method="post" enctype="multipart/form-data">
                 <section class="panel">
                     <header class="panel-heading">
-                        <div class="panel-actions">
-                            <a href="#" class="fa fa-caret-down"></a>
-                            <a href="#" class="fa fa-times"></a>
-                        </div>
-
                         <h2 class="panel-title">{{ $title }}</h2>
                         <p class="panel-subtitle">
                             {{ $description }}
