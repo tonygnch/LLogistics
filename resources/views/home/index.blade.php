1080fbe55c520b4822c703b69dc489c65f3fd57e
George <george@eurocoders.com>
2019-03-18 13:15:42 +0200

    update home page

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -518,57 +518,6 @@
                 </div>
             </section>
         </div>
-        <div class="col-xl-6 col-lg-12">
-            <section class="panel">
-                <header class="panel-heading panel-heading-transparent">
-                    <div class="panel-actions">
-                        <a href="#" class="fa fa-caret-down"></a>
-                        <a href="#" class="fa fa-times"></a>
-                    </div>
-
-                    <h2 class="panel-title">Company Activity</h2>
-                </header>
-                <div class="panel-body">
-                    <div class="timeline timeline-simple mt-xlg mb-md">
-                        <div class="tm-body">
-                            <div class="tm-title">
-                                <h3 class="h5 text-uppercase">November 2013</h3>
-                            </div>
-                            <ol class="tm-items">
-                                <li>
-                                    <div class="tm-box">
-                                        <p class="text-muted mb-none">7 months ago.</p>
-                                        <p>
-                                            It's awesome when we find a good solution for our projects, JSOFT Admin is
-                                            <span class="text-primary">#awesome</span>
-                                        </p>
-                                    </div>
-                                </li>
-                                <li>
-                                    <div class="tm-box">
-                                        <p class="text-muted mb-none">7 months ago.</p>
-                                        <p>
-                                            Checkout! How cool is that!
-                                        </p>
-                                        <div class="thumbnail-gallery">
-                                            <a class="img-thumbnail lightbox"
-                                               href="assets/images/projects/project-4.jpg"
-                                               data-plugin-options='{ "type":"image" }'>
-                                                <img class="img-responsive" width="215"
-                                                     src="assets/images/projects/project-4.jpg">
-                                                <span class="zoom">
-                                                                <i class="fa fa-search"></i>
-                                                            </span>
-                                            </a>
-                                        </div>
-                                    </div>
-                                </li>
-                            </ol>
-                        </div>
-                    </div>
-                </div>
-            </section>
-        </div>
     </div>
     <!-- end: page -->
 @endsection
