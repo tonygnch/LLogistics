8fa8cafa65ce77a487418d133435ffc5666d3801
George <george@eurocoders.com>
2019-03-27 00:09:41 +0200

    fix description

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,9 +4,8 @@
         <!-- Basic -->
         <meta charset="UTF-8">
 
-        <meta name="keywords" content="HTML5 Admin Template" />
-        <meta name="description" content="Porto Admin - Responsive HTML5 Template">
-        <meta name="author" content="okler.net">
+        <meta name="keywords" content="TransFluid Admin" />
+        <meta name="description" content="TransFluid Admin">
 
         <!-- Mobile Metas -->
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
