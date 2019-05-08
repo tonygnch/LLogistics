2912204ef52ad43c8110fbcd145e56e7304f0131
George <george@eurocoders.com>
2019-05-08 12:14:47 +0300

    test

--- app/Http/Controllers/Common/AjaxController.php
+++ app/Http/Controllers/Common/AjaxController.php
@@ -109,8 +109,8 @@ class AjaxController extends Controller
         }
 
         // Optional Authentication:
-        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-        curl_setopt($curl, CURLOPT_USERPWD, "username:password");
+//        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+//        curl_setopt($curl, CURLOPT_USERPWD, "username:password");
 
         curl_setopt($curl, CURLOPT_URL, $url);
         curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
